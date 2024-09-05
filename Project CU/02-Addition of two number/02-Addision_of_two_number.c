d22db0b7464b72b4c766df08ab41ce4be551951e
Jahid Hossain <107812081+Jahid-Hossain75@users.noreply.github.com>
2024-09-06 01:39:49 +0530

    Add files via upload

---
+++ Project CU/02-Addition of two number/02-Addision_of_two_number.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+// 01-Addition of two number
+int main() {
+   
+   int a, b, c;
+   printf("Enter a first number: ");
+   scanf("%d",&a);
+   printf("Enter a second number: ");
+   scanf("%d",&b);
+   c = a+b;
+   printf("Sum = %d", c);
+   
+    return 0;
+}
---
+++ Project CU/06-Switch Statement/06-Switch_Statement.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    char c = 'b';
+    switch(c){
+        case'd':
+            printf("Case D");
+            break;
+        
+        case'b':
+            printf("Case B");
+            break;
+        
+        case'c':
+            printf("Case C");
+            break;
+        
+        case'z':
+            printf("Case Z");
+            break;
+        
+        default:
+            printf("Default");
+    }
+    return 0;
+}
