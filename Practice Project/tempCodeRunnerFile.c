78ed1c5db3fa52b424290cf63591a7001a322ce4
Jahid Hossain <107812081+Jahid-Hossain75@users.noreply.github.com>
2024-09-06 01:23:57 +0530

    Delete Practice Project directory

--- Practice Project/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-int main(){
-//     int num1 = 20, num2 = 11, num3 = 10;
-//     int sum = num1 + num2 + num3;
-
-//     printf("Sum = %d\n", sum);
-//     printf("Average = %.2f\n", (float)sum / 3);
-//     return 0;
-// }
